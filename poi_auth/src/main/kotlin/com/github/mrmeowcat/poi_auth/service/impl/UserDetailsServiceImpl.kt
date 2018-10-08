84815cd5c21d5c9223f857f562982c4ed10d2779
MrMeowCat <viktorzablotsky@gmail.com>
2018-10-08 20:30:17 +0300

    implement user details service, fix database name in configs

--- poi_auth/src/main/kotlin/com/github/mrmeowcat/poi_auth/service/impl/UserDetailsServiceImpl.kt
+++ poi_auth/src/main/kotlin/com/github/mrmeowcat/poi_auth/service/impl/UserDetailsServiceImpl.kt
@@ -1,26 +1,32 @@
 package com.github.mrmeowcat.poi_auth.service.impl
 
+import com.github.mrmeowcat.poi_auth.dto.Account
+import com.github.mrmeowcat.poi_auth.service.AccountService
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
 /**
  * User details service implementation.
  */
 @Service
-class UserDetailsServiceImpl : UserDetailsService {
+class UserDetailsServiceImpl(private val accountService: AccountService) : UserDetailsService {
 
     /**
      * Loads user by username.
      */
     override fun loadUserByUsername(username: String): UserDetails {
-        return if (username == "admin") {
-            User(username, BCryptPasswordEncoder().encode("password"), listOf(SimpleGrantedAuthority("ROLE_ADMIN")))
+        if (!accountService.existsByUsername(username)) {
+            throw UsernameNotFoundException("username: $username")
         }
-        else throw UsernameNotFoundException("User not found!")
+        val account: Account = accountService.findByUsername(username)
+        val authorities: List<GrantedAuthority> = account.permissions.map {
+            SimpleGrantedAuthority(it)
+        }
+        return User(username, account.password, account.enabled, true, true, true, authorities)
     }
 }
