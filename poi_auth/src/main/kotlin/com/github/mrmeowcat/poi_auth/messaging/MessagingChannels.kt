f7cf00ad754c052377c62b497dc24e2102c620a8
MrMeowCat <viktorzablotsky@gmail.com>
2018-10-11 21:39:01 +0300

    add messaging/rabbitmq

---
+++ poi_auth/src/main/kotlin/com/github/mrmeowcat/poi_auth/messaging/MessagingChannels.kt
@@ -0,0 +1,14 @@
+package com.github.mrmeowcat.poi_auth.messaging
+
+/**
+ * Interface for messaging channels.
+ */
+interface MessagingChannels {
+
+    /**
+     * Channel names.
+     */
+    companion object {
+    }
+
+}
